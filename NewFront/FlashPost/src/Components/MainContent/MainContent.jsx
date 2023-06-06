d65e4949ce63b695a64427368d672b357a491abf
P-1000 <pavan.patchikarla@yahoo.com>
2023-06-06 16:22:26 +0530

    content Menu updated

--- NewFront/FlashPost/src/Components/MainContent/MainContent.jsx
+++ NewFront/FlashPost/src/Components/MainContent/MainContent.jsx
@@ -1,104 +1,80 @@
-import React from 'react'
-import BlogCardFooter from './BlogCardFooter'
-import BlogCards from './BlogCards'
-import ContentMenu from './ContentMenu'
-import { useEffect , useState } from 'react'
-import axios from 'axios'
-import  { Link } from 'react-router-dom'
-import { useParams } from 'react-router-dom'
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import BlogCardFooter from './BlogCardFooter';
+import BlogCards from './BlogCards';
+import ContentMenu from './ContentMenu';
 
 function MainContent() {
+  const token = localStorage.getItem('jwt');
+  const tok = JSON.parse(token);
+  const config = {
+    headers: { Authorization: `Bearer ${tok}` },
+  };
+  const [blogs, setBlogs] = useState([]);
+  const [category, setCategory] = useState('personalised');
 
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const response = await axios.get('http://localhost:3000/api/blogs/allBlogs', config);
+      const blog_data = response.data;
+      const rev = response.data.reverse();
+      setBlogs(rev);
+    };
+    fetchBlogs();
+  }, []);
 
-    const token = localStorage.getItem('jwt');
-    const tok = JSON.parse(token);
-    const config = {
-        headers: { Authorization: `Bearer ${tok}` }
-    }
-    const [blogs, setBlogs] = useState([]);
-    const [Trending, setTrending] = useState([]);
-    //fetching all blogs from database :
-    useEffect(()=>{
-      const fetchBlogs = async () => {
-        const response = await axios.get('http://localhost:3000/api/blogs/allBlogs', config);
-        console.log(response.data , "blog data ");
-        const blog_data = response.data
-        const rev = response.data.reverse()
-        setBlogs(rev);
-     //   trending(rev)
-      }
-      fetchBlogs();
-    },[])
-
-// blogs.map((blog)=>{
-//   blog.Author
-// })
-
-
-const trending = (blogs) => {
-  const blog = blogs
-  const trending = blog.sort((a,b)=> b.likes - a.likes)
-  setTrending(trending)
-}
-
-
+  const handleCategoryChange = (category) => {
+    // Update the category state variable this function is called in ContentMenu.jsx and passed as a prop to ContentMenu.jsx
+    setCategory(category);
+  };
 
   return (
     <>
-  <div className=''>
-    <div className='mt-3  pt-5'>
-    <div className='border rounded-md  bg-white   w-full font-bold text-primary py-6 '>
-        <div className=''>
-        <ContentMenu/>
-        </div>
-     
-   <div>
-      
-   {blogs && blogs.map((blog)=>{
-      return(
-    
-          <div className='border-b-[1px]'>
-        <div>
-      <div>
-      <Link to={`/blog/@${blog.Author}/${blog._id}`}>
-                <BlogCards 
-                Author={blog.Author}
-                desc= {blog.desc}
-                title={blog.title}
-                imgUrl={blog.imgUrl}
-                blog_id={blog._id}
-                time={blog.createdAt}
-                 />
-                   </Link>
+      <div className=''>
+        <div className='mt-3  pt-5'>
+          <div className='border rounded-md  bg-white   w-full font-bold text-primary py-6 '>
+            <div className=''>
+              <ContentMenu category={category} onCategoryChange={handleCategoryChange} /> 
+              {/* // passing the function as a prop to ContentMenu.jsx */}
             </div>
-         {
-            blog.tags && <div className='mb-3'>
-            <BlogCardFooter
-            id={blog._id}
-            like={blog.likes}
-             tag={blog.tags} />
+
+            <div>
+              {blogs &&
+                blogs.map((blog) => {
+                  // Filter blogs based on the selected category
+                  if (category === 'personalised' || blog.category === category) {
+                    return (
+                      <div className='border-b-[1px]'>
+                        <div>
+                          <div>
+                            <Link to={`/blog/@${blog.Author}/${blog._id}`}>
+                              <BlogCards
+                                Author={blog.Author}
+                                desc={blog.desc}
+                                title={blog.title}
+                                imgUrl={blog.imgUrl}
+                                blog_id={blog._id}
+                                time={blog.createdAt}
+                              />
+                            </Link>
+                          </div>
+                          {blog.tags && (
+                            <div className='mb-3'>
+                              <BlogCardFooter id={blog._id} like={blog.likes} tag={blog.tags} />
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                    );
+                  }
+                })}
+            </div>
+          </div>
         </div>
-         }
-            </div> </div> 
-          
-      )
-    })}
-   </div>
-
-
-        
-
-     
-        
-    </div>
-
-    
-    </div>
-  </div>
- 
- </>
-  )
+      </div>
+    </>
+  );
 }
 
-export default MainContent
+export default MainContent;
