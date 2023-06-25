45b72c9483f4ce54d263aba755db176665821749
P-1000 <pavan.patchikarla@yahoo.com>
2023-06-25 18:36:59 +0530

    mblog

--- Server/index.js
+++ Server/index.js
@@ -32,9 +32,9 @@ app.use(cors({
 
 const connect = () =>{
     console.log("waiting for the Db")
-    // mongoose.connect(process.env.MONGO).then(()=>{
-    //     console.log("Database connected")
-    // }).catch(err => {throw err})
+    mongoose.connect(process.env.MONGO).then(()=>{
+        console.log("Database connected")
+    }).catch(err => {throw err})
 }
 
 
