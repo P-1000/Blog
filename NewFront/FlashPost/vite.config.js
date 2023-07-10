13268ec2ffe0813ce6ecfc58cd3960d63d08302f
P-1000 <pavan.patchikarla@yahoo.com>
2023-07-10 23:08:43 +0530

    csr

--- NewFront/FlashPost/vite.config.js
+++ NewFront/FlashPost/vite.config.js
@@ -1,11 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { resolve } from 'path'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, 'index.html'),
+//         // nested: resolve(__dirname, 'nested/index.html'),
+//       },
+//     },
+//   },
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          main: '/src/index.js',
+        },
+      },
+    },
+    server: {
+      // Adjust the server configuration as needed
+      // For example, specify the port and set up a proxy if necessary
+      port: 5173,
+    },
+    optimizeDeps: {
+      include: ['react-router-dom'],
+    },
+    resolve: {
+      alias: {
+        // Define any custom aliases as needed
+      },
+    },
+  };
+});
+
 
 
 
