85760d9f6998b4fda543850e79f14eb7532376f4
P-1000 <pavan.patchikarla@yahoo.com>
2023-07-10 23:14:54 +0530

    build config

--- NewFront/FlashPost/vite.config.js
+++ NewFront/FlashPost/vite.config.js
@@ -1,21 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import { resolve } from 'path'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     rollupOptions: {
-//       input: {
-//         main: resolve(__dirname, 'index.html'),
-//         // nested: resolve(__dirname, 'nested/index.html'),
-//       },
-//     },
-//   },
-// })
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -26,13 +8,11 @@ export default defineConfig(({ mode }) => {
     build: {
       rollupOptions: {
         input: {
-          main: '/src/index.js',
+          main: '/index.html',
         },
       },
     },
     server: {
-      // Adjust the server configuration as needed
-      // For example, specify the port and set up a proxy if necessary
       port: 5173,
     },
     optimizeDeps: {
@@ -45,24 +25,3 @@ export default defineConfig(({ mode }) => {
     },
   };
 });
-
-
-
-
-// import { defineConfig } from 'vite';
-// import { createReactPlugin } from 'vite-plugin-react';
-// import ssr from 'vite-plugin-ssr/plugin';
-
-// export default defineConfig({
-//   plugins: [
-//     createReactPlugin(),
-//     ssr({
-//       entry: './src/entry-server.js',
-//       clientEntry: './src/entry-client.js',
-//       serverOutDir: 'dist/server',
-//       clientOutDir: 'dist/client',
-//       // Additional options for the plugin
-//       // ...
-//     }),
-//   ],
-// });
