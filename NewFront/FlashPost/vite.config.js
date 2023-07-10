b684ff8c1589653c8c3f3862930ecef110765fa8
P-1000 <pavan.patchikarla@yahoo.com>
2023-07-10 14:13:15 +0530

    routing

--- NewFront/FlashPost/vite.config.js
+++ NewFront/FlashPost/vite.config.js
@@ -6,3 +6,23 @@ export default defineConfig({
   plugins: [react()],
   
 })
+
+
+
+// import { defineConfig } from 'vite';
+// import { createReactPlugin } from 'vite-plugin-react';
+// import ssr from 'vite-plugin-ssr/plugin';
+
+// export default defineConfig({
+//   plugins: [
+//     createReactPlugin(),
+//     ssr({
+//       entry: './src/entry-server.js',
+//       clientEntry: './src/entry-client.js',
+//       serverOutDir: 'dist/server',
+//       clientOutDir: 'dist/client',
+//       // Additional options for the plugin
+//       // ...
+//     }),
+//   ],
+// });
