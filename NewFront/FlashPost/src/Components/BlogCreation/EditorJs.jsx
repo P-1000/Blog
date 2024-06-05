5ccc624320e32c2a165be1f069ef8d8e78bf14fc
p-1000 <patchikarla.12108354@lpu.in>
2024-06-05 10:19:37 +0530

    Fix: Upgrade Vercel deployment and add hooks for editor
    Updates and fixes:
    
    - Upgrades Vercel deployment to use the latest version of Next.js.
    - Updates EditorJs in the text editor to add hooks for saving and retrieving content.
    - Removes unnecessary files and rewrites from Vercel.json.

--- NewFront/FlashPost/src/Components/BlogCreation/EditorJs.jsx
+++ NewFront/FlashPost/src/Components/BlogCreation/EditorJs.jsx
@@ -1,22 +1,18 @@
-import React, { useEffect, useRef } from 'react';
-import EditorJS from '@editorjs/editorjs';
-import Header from '@editorjs/header';
-import List from '@editorjs/list';
-import Table from '@editorjs/table';
-import ImageTool from '@editorjs/image';
-import ImageKit from 'imagekit';
-import './TextEditor.css'; 
-import { useDispatch } from 'react-redux';
-import { updateContent } from '../../redux/formDataSlice';
-import CodeTool from '@editorjs/code';
+import React, { useEffect, useRef } from "react";
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
+import Table from "@editorjs/table";
+import ImageTool from "@editorjs/image";
+import ImageKit from "imagekit";
+import "./TextEditor.css";
+import { useDispatch } from "react-redux";
+import { updateContent } from "../../redux/formDataSlice";
+import CodeTool from "@editorjs/code";
 
-
-
-const EDITTOR_HOLDER_ID = 'editorjs';
+const EDITTOR_HOLDER_ID = "editorjs";
 
 const TextEditor = (props) => {
-
-
   const ejInstance = useRef();
   const [editorData, setEditorData] = React.useState(props.data);
 
@@ -30,25 +26,26 @@ const TextEditor = (props) => {
     };
   }, []);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(updateContent(editorData));
-        props.setdata(editorData)
-    }, [editorData]);
-    
+  console.log(editorData);
+
+  useEffect(() => {
+    dispatch(updateContent(editorData));
+    props.setdata(editorData);
+  }, [editorData]);
 
   const initEditor = () => {
     const imagekit = new ImageKit({
-      publicKey: 'public_URvjzrf8cUDwCO0A6NK3VOYWg1U=',
-      urlEndpoint: 'https://ik.imagekit.io/cwq19b8fi',
-      privateKey: 'private_gR6kfpKknhbtLmBe7OXtwKJ19h0=',
-      authenticationEndpoint: 'http://localhost:5173/auth',
+      publicKey: "public_URvjzrf8cUDwCO0A6NK3VOYWg1U=",
+      urlEndpoint: "https://ik.imagekit.io/cwq19b8fi",
+      privateKey: "private_gR6kfpKknhbtLmBe7OXtwKJ19h0=",
+      authenticationEndpoint: "http://localhost:5173/auth",
     });
 
     const editor = new EditorJS({
       holder: EDITTOR_HOLDER_ID,
-      logLevel: 'ERROR',
+      logLevel: "ERROR",
       data: editorData,
       onReady: () => {
         ejInstance.current = editor;
@@ -62,14 +59,14 @@ const TextEditor = (props) => {
         header: {
           class: Header,
           config: {
-            placeholder: 'Enter a heading...',
+            placeholder: "Enter a heading...",
             levels: [1, 2, 3, 4],
           },
           defaultLevel: 2,
         },
         list: List,
         table: Table,
-        code : CodeTool,
+        code: CodeTool,
         image: {
           class: ImageTool,
           inlineToolbar: false,
@@ -80,7 +77,7 @@ const TextEditor = (props) => {
                   const result = await imagekit.upload({
                     file: file,
                     fileName: file.name,
-                    folder: '/editor-images',
+                    folder: "/editor-images",
                   });
 
                   return {
@@ -90,17 +87,16 @@ const TextEditor = (props) => {
                     },
                   };
                 } catch (error) {
-                  console.error('Error uploading image:', error);
+                  console.error("Error uploading image:", error);
                   return {
                     success: 0,
-                    message: 'Error uploading image',
+                    message: "Error uploading image",
                   };
                 }
               },
             },
           },
         },
-        // Add more tools as needed
       },
       i18n: {
         messages: {},
@@ -109,9 +105,13 @@ const TextEditor = (props) => {
   };
 
   return (
-    <div className='pt-24 bg-white h-full w-full min-h-[100vh]'>
-    <button className='mx-10 my-2 border px-4  hover:bg-black hover:px-6 rounded-sm hover:rounded-md hover:text-white transition-all duration-300 ease-in-out'
-    onClick={props.onBack}>Back</button>
+    <div className="pt-24 bg-white h-full w-full min-h-[100vh]">
+      <button
+        className="mx-10 my-2 border px-4  hover:bg-black hover:px-6 rounded-sm hover:rounded-md hover:text-white transition-all duration-300 ease-in-out"
+        onClick={props.onBack}
+      >
+        Back
+      </button>
       <div id={EDITTOR_HOLDER_ID}></div>
       <style>
         {`
@@ -156,12 +156,8 @@ const TextEditor = (props) => {
           }
         `}
       </style>
-      
     </div>
   );
 };
 
 export default TextEditor;
-
-
-
