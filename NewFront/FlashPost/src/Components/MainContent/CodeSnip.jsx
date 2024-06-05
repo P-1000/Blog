d8baee0c81400cecdc72d4c8b8122ca12ebe1011
p-1000 <patchikarla.12108354@lpu.in>
2024-06-05 17:56:40 +0530

    roll back point for now

--- NewFront/FlashPost/src/Components/MainContent/CodeSnip.jsx
+++ NewFront/FlashPost/src/Components/MainContent/CodeSnip.jsx
@@ -1,25 +1,25 @@
 import React, { useEffect, useRef } from "react";
-import Prism from "prismjs";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+// import Prism from "prismjs";
+// import SyntaxHighlighter from "react-syntax-highlighter";
+// import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 // Import the specific language support from Prism.js if needed
-import "prismjs/components/prism-javascript";
+// import "prismjs/components/prism-javascript";
 
 const CodeSnippet = ({ code }) => {
-  const codeRef = useRef(null);
+  // const codeRef = useRef(null);
 
-  useEffect(() => {
-    // Ensure the codeRef is available
-    if (codeRef.current) {
-      // Highlight the code using Prism
-      Prism.highlightElement(codeRef.current);
-    }
-  }, [code]);
+  // useEffect(() => {
+  //   // Ensure the codeRef is available
+  //   if (codeRef.current) {
+  //     // Highlight the code using Prism
+  //     Prism.highlightElement(codeRef.current);
+  //   }
+  // }, [code]);
 
   return (
     <div className="font-light">
-      <SyntaxHighlighter language="javascript"  theme={dark}  >{code}</SyntaxHighlighter>
+      {/* <SyntaxHighlighter language="javascript"  theme={dark}  >{code}</SyntaxHighlighter> */}
     </div>
   );
 };
--- NewFront/FlashPost/vite.config.js
+++ NewFront/FlashPost/vite.config.js
@@ -15,7 +15,6 @@ export default defineConfig(({ mode }) => {
     optimizeDeps: {
       include: [
         'react-router-dom',
-        'react-syntax-highlighter'  
       ],
     },
     resolve: {
