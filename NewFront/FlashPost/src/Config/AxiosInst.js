82010e639e69098a180bd87be783e5af7dd9a731
p-1000 <patchikarla.12108354@lpu.in>
2024-06-05 11:26:59 +0530

    navigate

--- NewFront/FlashPost/src/Config/AxiosInst.js
+++ NewFront/FlashPost/src/Config/AxiosInst.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const url = 'https://back-e0rl.onrender.com';
-const url = 'http://localhost:3000';
+const url = 'https://back-e0rl.onrender.com';
+// const url = 'http://localhost:3000';
 
 const instance = axios.create({
     baseURL: url,
