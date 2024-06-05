444229d22b2e30b14aae93e6e764383ceca78a88
p-1000 <patchikarla.12108354@lpu.in>
2024-06-05 17:52:31 +0530

    roll back point for now

--- NewFront/FlashPost/vite.config.js
+++ NewFront/FlashPost/vite.config.js
@@ -1,29 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     build: {
       rollupOptions: {
         external: [
-          '@editorjs/code' ,
-          'react-syntax-highlighter'
+          '@editorjs/code'
+          // 'react-syntax-highlighter' is removed from here
         ],
       },
     },
-    server: {
-      port: 5173,
-      proxy: {
-        '/api': {
-          target: 'http://localhost:3000',
-          changeOrigin: true,
-        },
-      },
-    },
     optimizeDeps: {
-      include: ['react-router-dom'],
+      include: [
+        'react-router-dom',
+        'react-syntax-highlighter'  
+      ],
     },
     resolve: {
       alias: {
