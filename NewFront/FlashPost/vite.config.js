be22f0d426ec58d359480b55391931f2651e07eb
p-1000 <patchikarla.12108354@lpu.in>
2024-06-05 18:00:18 +0530

    roll back point for now

--- NewFront/FlashPost/vite.config.js
+++ NewFront/FlashPost/vite.config.js
@@ -1,25 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     build: {
-      rollupOptions: {
-        external: [
-          '@editorjs/code'
-          // 'react-syntax-highlighter' is removed from here
-        ],
-      },
+      rollupOptions: {},
     },
     optimizeDeps: {
-      include: [
-        'react-router-dom',
-      ],
+      include: ["react-router-dom", "@editorjs/code"],
     },
     resolve: {
       alias: {
-        
       },
     },
   };
